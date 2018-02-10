8e8ab8dc8811fc0005262af5bf3bf6ba287f45f3
David Butler <dbutler992@gmail.com>
2018-02-10 17:59:57 -0600

    Added Develop branch and started to work on grid generation

--- sketch.js
+++ sketch.js
@@ -1,10 +1,20 @@
+var width = 500;
+var height = 500;
+
 function setup() {
-	createCanvas(400,300);
+	createCanvas(500,500);
 }
 
 function draw() {
-	background(140,10, 10);
-	stroke(255);
-	fill(255);
+	background(0,120,120);
+	stroke(125);
+	fill(120);
 	rect(30, 20, 55, 55);
+}
+
+function generateGrid(){
+    for(i = 0; i < height; i++){
+        for(j = 0; j < width; j++){
+           }
+    }
 }
