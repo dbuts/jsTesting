09ab1b11d25a855875e7bb546a9282eca6328023
David Butler <dbutler992@gmail.com>
2018-02-13 20:36:28 -0600

    Added libraries

--- sketch.js
+++ sketch.js
@@ -5,12 +5,13 @@ var cells = [];
 function setup() {
 	createCanvas(500,500);
     generateGrid(); 
-    console.log("Rows: " + cells.length);
-    console.log("Cols: " + cells[0].length);
+//    console.log("Rows: " + cells.length);
+ //   console.log("Cols: " + cells[0].length);
 }
 
 function draw() {
     background(51);
+    update();
     drawGrid();
 }
 
@@ -22,6 +23,10 @@ function generateGrid(){
         }  
         cells.push(currRow); 
     }
+    cells[0][0].state = 1;
+    //cells[0][1].state = 1;
+    //cells[0][2].state = 1;
+    cells[1][0].state = 1;
 }
 
 function drawGrid(){
@@ -40,8 +45,69 @@ function drawGrid(){
      }
 }
 
-function cell(state, x, y){
-    this.state = state;
+function update(){
+    var curCell;
+    for(i = 0; i<rows; i++){
+        for(j = 0; j<cols; j++){
+            if(i<50){
+            //console.log("CurrCell i: " + i + " j: " +j);
+            currCell = cells[i][j]; 
+            if(currCell.state == 1){
+                switch(aliveNeighbors(currCell)){
+                    case 0: // Underpopulation
+                    case 1: // Underpopulation
+                    case 4: // Overpopulation
+                    case 5: // Overpopulation
+                    case 6: // Overpopulation
+                    case 7: // Overpopulation
+                    case 8: // Overpopulation
+                        currCell.state = 0;
+                }
+            }
+            if(currCell.state == 0){
+                if(aliveNeighbors(currCell) == 3){
+                    currCell.state = 1;
+                }
+            }
+            }
+        }
+    }
+}
+
+function aliveNeighbors(cell){
+    var rtn = 0;
+    minX = cell.x -1;
+    maxX = cell.x +1;
+    minY = cell.y -1;
+    maxY = cell.y +1;
+    if(minX < 0){ minX = 0;}
+    if(maxX > rows-1){maxX = rows-1;}
+    if(minY < 0){ minY = 0;}
+    if(maxY > cols-1){maxY = cols - 1;}
+
+    var count = 0;
+    for(i = minX; i<=maxX; i++){
+       for(j = minY; j<=maxY; j++){
+           if(cell.x == i && cell.y == j){
+             //  console.log("Current Cell: " + i + ", " +j);
+           }
+           else{
+              // console.log("X: " + cell.x + "Y: " + cell.y);
+              count++;
+              console.log(count);
+               if(cells[i][j].state == 1){
+                   rtn+=1;
+               }
+           }
+       }
+    }
+    console.log("Neighbors: " + rtn);
+    return rtn;
+}
+
+function cell(stat, x, y){
+    this.state = 0;
+    this.state = stat; // Alive = 1, Dead = 0
     this.x = x;
     this.y = y;
 }
